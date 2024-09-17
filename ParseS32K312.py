f4f11a2b6e6692fe10666b039a3a66c13cab33db
Peter Chen <chenwilsoft@msn.com>
2024-09-17 10:44:04 +0200

    Update filename algorithm

--- ParseS32K312.py
+++ ParseS32K312.py
@@ -5,12 +5,12 @@ from S32K3.DeviceS32K312 import DeviceS32K312
 
 
 def main():
-    if len(sys.argv) != 3:
-        print("使用方法: python ParseS32K312.py <输入文件名> <输出Excel文件名>")
+    if len(sys.argv) != 2:
+        print("使用方法: python ParseS32K312.py <输入文件名>")
         sys.exit(1)
 
     input_filename = sys.argv[1]
-    excel_filename = sys.argv[2]
+    excel_filename = input_filename.rsplit('.', 1)[0] + '.xlsx'
 
     Parse(input_filename, excel_filename, DeviceS32K312())
 
